730cd1fe614338a3fb0881f72b51d0ad162e6e2d
Ni8crawler18 <ni8crawler12@gmail.com>
2025-03-11 21:07:41 +0530

    changed settings page

--- client/src/pages/SettingsPage.tsx
+++ client/src/pages/SettingsPage.tsx
@@ -262,7 +262,7 @@ const SettingsPage: React.FC = () => {
                     <div className="flex-shrink-0">
                       <img
                         className="h-16 w-16 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src="public/user_logo.jpeg"
                         alt="User avatar"
                       />
                     </div>
